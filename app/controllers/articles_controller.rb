d3f95e84fb9b1fd9c148a5099f27494d16e67bf0
Sazami4423 <78459786+Sazami4423@users.noreply.github.com>
2021-03-25 03:02:01 +0900

    投稿機能の実装

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,21 +1,27 @@
 class ArticlesController < ApplicationController
 
   def index
+    @articles = Article.all
   end
 
   def new
+    @article = Article.new
   end
 
   def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to articles_path
+    else
+      render :new
+    end
+ 
   end
 
-  def edit
-  end
+  private
 
-  def update
-  end
-
-  def destroy
+  def article_params
+    params.require(:article).permit(:title, :text)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'homes#index'
-  resources :articles, only: [:index, :new, :create, :edit, :update, :destroy]
+  resources :articles, only: [:index, :new, :create]
 end
